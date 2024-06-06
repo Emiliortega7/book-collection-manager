29d8f6748ad3e80a40cf557e8c8d2cc2a18538b2
Emiliortega7 <128631724+Emiliortega7@users.noreply.github.com>
2024-06-06 23:39:11 +0300

    optimization

--- models.py
+++ models.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
+from flask_caching import Cache
 
 load_dotenv('.env')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Configure Flask-Caching
+app.config['CACHE_TYPE'] = 'simple'  # Consider using 'redis' or 'memcached' for production
 
 db = SQLAlchemy(app)
+cache = Cache(app)
 
 class Book(db.Model):
     __tablename__ = 'books'
@@ -18,3 +22,7 @@ class Book(db.Model):
     author = db.Column(db.String(30), nullable=False)
     published_year = db.Column(db.Integer)
     genre = db.Column(db.String(50))
+
+@cache.memoize(timeout=60)  # Cache this result for 60 seconds
+def get_books_by_genre(genre):
+    return Book.query.filter_by(genre=genre).all()
